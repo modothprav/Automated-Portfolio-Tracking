3eec4741f926ddc557286d2ea83cd8ee5318e737
Pravin Modotholi <pravinmanoj44@gmail.com>
2021-07-06 20:00:08 +1200

    feat(Report): enter details and export transaction report
    Methods added which allows the selection of a period duration
    and the report type to be exported

--- src/main/java/sharesies/SharesiesReports.java
+++ src/main/java/sharesies/SharesiesReports.java
@@ -3,6 +3,7 @@ package sharesies;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import base.BasePage;
 
@@ -28,6 +29,9 @@ public class SharesiesReports extends BasePage {
     private WebElement exportButton;
 
 
+    /**
+     * Constructor
+     */
     public SharesiesReports() {
         if (!driver.getTitle().equals("Sharesies")) {
             throw new IllegalStateException("This is not the Sharesies Home Page, current Page is " 
@@ -35,5 +39,67 @@ public class SharesiesReports extends BasePage {
         }
 
         PageFactory.initElements(driver, this);
+    }
+
+    /**
+     * Select the starting Month period of the transaction hisroty.
+     * Specify the full month with the correct spelling starting 
+     * with a capital letter e.g. January
+     * 
+     * @param month String which represents a month of the year
+     */
+    public void selectFromMonth(String month) {
+        Select fromMonth = new Select(this.fromMonthField);
+        fromMonth.selectByVisibleText(month);
+    }
+    
+    /**
+     * Select the ending Month period of the transaction hisroty.
+     * Specify the full month with the correct spelling starting 
+     * with a capital letter e.g. January
+     * 
+     * @param month String which represents a month of the year
+     */
+    public void selectToMonth(String month) {
+        Select toMonth = new Select(this.toMonthField);
+        toMonth.selectByVisibleText(month);
+    }
+
+    /**
+     * Select the starting Year period of the transaction hisroty.
+     * Specify the full year e.g. 2021
+     * 
+     * @param year String which represents the year
+     */
+    public void selectFromYear(String year) {
+        Select fromYear = new Select(this.fromYearField);
+        fromYear.selectByVisibleText(year);
+    }
+
+    /**
+     * Select the ending Year period of the transaction hisroty.
+     * Specify the full year e.g. 2021
+     * 
+     * @param year String which represents the year
+     */
+    public void selectToYear(String year) {
+        Select toYear = new Select(this.toYearField);
+        toYear.selectByVisibleText(year);
+    }
+
+    /**
+     * Click the CSV Report export type on the reports page 
+     */
+    public void clickCSVReport() {
+        this.csvReport.click();
+    }
+
+    /**
+     * If the period infromation and report type have all been 
+     * entered the export button can be clicked to export the 
+     * requested report type within the given period. 
+     */
+    public void clickExport() {
+        this.exportButton.click();
     }
 }
