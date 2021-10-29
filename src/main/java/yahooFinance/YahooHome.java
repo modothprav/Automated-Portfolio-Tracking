05bf81f368b6c1bee1763782533538770b6b9f73
Pravin Modotholi <pravinmanoj44@gmail.com>
2021-10-29 21:28:56 +1300

    feat: initial Yahoo Home page object model built

--- src/main/java/yahooFinance/YahooHome.java
+++ src/main/java/yahooFinance/YahooHome.java
@@ -1,5 +1,55 @@
 package yahooFinance;
 
-public class YahooHome {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import base.BasePage;
+
+public class YahooHome extends BasePage{
+
+    // Object Repository
+    @FindBy(id = "root_3")
+    private WebElement financeLink;
+
+    @FindBy(id = "profile-signout-link")
+    private WebElement signOutLink;
+
+    @FindBy(id = "ybarAccountMenuOpener")
+    private WebElement profileButton;
+
+    @FindBy(xpath = "//*[text()='Sign in']")
+    private WebElement signInButton;
+
+    /**
+     * Constructor
+     */
+    public YahooHome() {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void goToYahooFinance() {
+        this.financeLink.click();
+    }
+
+    private void clickProfile() {
+        this.profileButton.click();
+    }
+
+    private void clickSignOut() {
+        this.signOutLink.click();
+    }
+
+    public YahooHome signOut() {
+        this.clickProfile();
+        this.clickSignOut();
+        return new YahooHome();
+    }
+
+    public YahooLogin clickSignIn() {
+        signInButton.click();
+        return new YahooLogin();
+    }
+    
     
 }
