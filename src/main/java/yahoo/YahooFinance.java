0b75b8f11cf81fba0d21a43fbfd05c2fe2fb57bf
Pravin Modotholi <pravinmanoj44@gmail.com>
2021-10-30 21:12:06 +1300

    feat: page objects for Yahoo finance page built

--- src/main/java/yahoo/YahooFinance.java
+++ src/main/java/yahoo/YahooFinance.java
@@ -1,7 +1,52 @@
 package yahoo;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import base.BasePage;
 
 public class YahooFinance extends BasePage {
     
+    // Object Repository
+    @FindBy(xpath = "//a[@href='/portfolios']")
+    private WebElement portfolioLink;
+
+    @FindBy(id = "uh-profile")
+    private WebElement profileButton;
+
+    @FindBy(id = "uh-signedout")
+    private WebElement signOutButton;
+
+    @FindBy(id = "uh-signedin")
+    private WebElement signInButton;
+
+    public YahooFinance() {
+        if (!driver.getTitle().equals("Yahoo Finance – stock market live, quotes, business & finance news")) {
+            throw new IllegalStateException("This is not the Yahoo Finance Page, current Page is " 
+            + driver.getCurrentUrl()); 
+        }
+
+        PageFactory.initElements(driver, this);
+    }
+
+    public void goToPortfolioPage() {
+        this.portfolioLink.click();
+    }
+
+    private void clickProfile() {
+        this.profileButton.click();
+    }
+
+    private void clickSignOut() {
+        this.signOutButton.click();
+    }
+
+    public YahooHome signOut() {
+        this.clickProfile();
+        this.clickSignOut();
+        return new YahooHome();
+    }
+
+
 }
