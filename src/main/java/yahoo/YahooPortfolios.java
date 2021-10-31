08a4259007c0fa894fad393cad890227f388ff8c
Pravin Modotholi <pravinmanoj44@gmail.com>
2021-10-31 20:11:04 +1300

    feat: Portfolios page built, config property added
    Fixed loading error, added another condition
    in YahooFinance.java to wait for DOM elements
    to laod.

--- src/main/java/yahoo/YahooPortfolios.java
+++ src/main/java/yahoo/YahooPortfolios.java
@@ -1,5 +1,23 @@
 package yahoo;
 
-public class YahooPortfolios {
+import org.openqa.selenium.By;
 
+import base.BasePage;
+
+
+public class YahooPortfolios extends BasePage {
+
+    private final String name = config.getProperty("yahoo.portfolio.name");
+
+    public YahooPortfolios() {
+        if (!driver.getTitle().equals("Stock portfolio & tracker – Yahoo Finance")) {
+            throw new IllegalStateException("This is not the Yahoo Portfolios Page, current Page is " 
+            + driver.getCurrentUrl()); 
+        }
+    }
+
+    public void clickPortfolio() {
+        driver.findElement(By.xpath("//a[text()='" + name +  "']")).click();
+    }
+    
 }
