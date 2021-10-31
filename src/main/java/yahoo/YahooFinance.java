6479b61e49f8fb4b95400e24e660f66a747b54f6
Pravin Modotholi <pravinmanoj44@gmail.com>
2021-10-31 20:24:45 +1300

    docs: method docs added and refactored to POM structure

--- src/main/java/yahoo/YahooFinance.java
+++ src/main/java/yahoo/YahooFinance.java
@@ -1,11 +1,8 @@
 package yahoo;
 
-import java.time.Duration;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
--- src/main/java/yahoo/YahooPortfolios.java
+++ src/main/java/yahoo/YahooPortfolios.java
@@ -7,8 +7,11 @@ import base.BasePage;
 
 public class YahooPortfolios extends BasePage {
 
-    private final String name = config.getProperty("yahoo.portfolio.name");
+    private final String portfolioName = config.getProperty("yahoo.portfolio.name");
 
+    /**
+     * Constructor
+     */
     public YahooPortfolios() {
         if (!driver.getTitle().equals("Stock portfolio & tracker – Yahoo Finance")) {
             throw new IllegalStateException("This is not the Yahoo Portfolios Page, current Page is " 
@@ -16,8 +19,14 @@ public class YahooPortfolios extends BasePage {
         }
     }
 
-    public void clickPortfolio() {
-        driver.findElement(By.xpath("//a[text()='" + name +  "']")).click();
+    /**
+     * Clicks the Portfolio specifed in the config file.
+     * Returns a page containing the portfolio data
+     * @return YahooPortfolioData
+     */
+    public YahooPortfolioData clickPortfolio() {
+        driver.findElement(By.xpath("//a[text()='" + portfolioName +  "']")).click();
+        return new YahooPortfolioData();
     }
     
 }
