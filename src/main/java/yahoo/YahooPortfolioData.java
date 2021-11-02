2868e8c7bf18a104cb2e827f1f42a7ae14a99537
Pravin Modotholi <pravinmanoj44@gmail.com>
2021-11-02 21:22:27 +1300

    feat: scrapes individual stock rows from holdings sections #28
    Since most of this page is dynamic, page objects
    are not initialized upon construction. Hence they
    are queried using dynamic Xpath.

--- src/main/java/yahoo/YahooPortfolioData.java
+++ src/main/java/yahoo/YahooPortfolioData.java
@@ -1,7 +1,48 @@
 package yahoo;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import base.BasePage;
 
-public class YahooPortfolioData extends BasePage{
+public class YahooPortfolioData extends BasePage {
+
+    // Object Repository
+    @FindBy(xpath = "//span[text()='My holdings']")
+    private WebElement holdingsTab;
+
+    // @FindAll({@FindBy(xpath = "//table/tbody[*]/tr[2]/td/button")})
+    // private List<WebElement> dropdownButtons;
     
+    // @FindAll({@FindBy(xpath = "//table/tbody[*]/tr[2]/td[2]/div/a")})
+    // private List<WebElement> stockTickers;
+
+
+    /**
+     * Constructor
+     */
+    public YahooPortfolioData() {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void clickHoldingsTab() {
+        this.holdingsTab.click();
+    }
+
+    public void clickDropdown(int stockRow) {
+        WebElement button = driver.findElement(By.xpath("//table/tbody["+ stockRow +"]/tr[2]/td/button"));
+        button.click();
+    }
+
+    public List<WebElement> getStockHoldings() {
+        return driver.findElements(By.xpath("//table/tbody/*"));
+    }
+
+
+
 }
